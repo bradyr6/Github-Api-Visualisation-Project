932233e7a89ad93a91ec3f1f46049f82132c788d
Rachel Brady <60605729+bradyr6@users.noreply.github.com>
2021-01-02 17:30:26 +0000

    Add files via upload
    Initial api access and react setup

---
+++ build/precache-manifest.b1a72c3677ffe67e5210b3bf37e3a71d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6693f98ec78789bdf18df4656bd8e095",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22f60fc30b0e2714a390",
+    "url": "/static/css/2.0b76d70b.chunk.css"
+  },
+  {
+    "revision": "837ce7fe4883c69e18f9",
+    "url": "/static/css/main.d90ef3c1.chunk.css"
+  },
+  {
+    "revision": "22f60fc30b0e2714a390",
+    "url": "/static/js/2.c5a6a2c2.chunk.js"
+  },
+  {
+    "revision": "837ce7fe4883c69e18f9",
+    "url": "/static/js/main.ea29f316.chunk.js"
+  },
+  {
+    "revision": "87b97767a3688a18746d",
+    "url": "/static/js/runtime-main.26f34a92.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
---
+++ components/graph.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Plot from 'react-plotly.js';
+
+const graph = (props) => {
+        const repoSize = [];
+        const repoNames = [];
+        const languageList = [];
+        const languageCount = [];
+        for(var i = 0; i < props.repositories.length; i++){
+            repoSize.push(props.repositories[i].size);
+            repoNames.push(props.repositories[i].name); 
+        }
+        for(var i = 0; i < props.repositories.length; i++){
+            if(props.repositories[i].language != null) {
+                if(languageList.indexOf(props.repositories[i].language) === -1) {
+                  languageList.push(props.repositories[i].language);
+                }
+            }
+        }
+        for(var i = 0; i < languageList.length; i++) {
+            languageCount[i] = 0;
+        }
+        for(var i = 0; i < props.repositories.length; i++) {
+            for(var j = 0; j < languageList.length; j++) {
+                if(props.repositories[i].language === languageList[j]) {
+                    languageCount[j]++;
+                }
+            }
+        }
+        
+        return (
+            <div class = "row">
+                <div>
+                    <Plot data={[
+                        {
+                        values: repoSize,
+                        labels: repoNames,
+                        type: 'pie'
+                        }
+                    ]} layout={{width: 500, height: 300}}/>
+                </div>
+                <div>
+                <Plot data={[
+                        {
+                        values: languageCount,
+                        labels: languageList,
+                        type: 'pie'
+                        }
+                    ]} layout={{width: 500, height: 300}}/>
+                </div>
+            </div>         
+
+        )
+
+};
+
+export default graph;
